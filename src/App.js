be647a68c243d5a2c739c885f01cf40ace8f67a3
Kanem <kanemdeng@email.com>
2023-02-02 21:56:59 -0800

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Box, Paper, Typography } from '@mui/material'
+import Grid from '@mui/material/Unstable_Grid2'
+import './App.css'
+import FriendList from './components.js/FriendList'
+import Messages from './components.js/Messages'
+import { useEffect, useRef } from 'react'
+import { render } from '@testing-library/react'
 
 function App() {
+
+  const num = useRef(0)
+
+  useEffect(() => {
+    num.current++
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Box sx={{ width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <Typography>main app render: {num.current}</Typography>
+      <Box >
+        <Paper sx={{ width: '80vw', height: '80vh' }}>
+          <Grid container>
+            <Grid xs={4}>
+              <FriendList />
+            </Grid>
+            <Grid xs={8}>
+              <Messages />
+            </Grid>
+          </Grid>
+        </Paper>
+      </Box>
+    </Box >
+  )
 }
 
-export default App;
+export default App
